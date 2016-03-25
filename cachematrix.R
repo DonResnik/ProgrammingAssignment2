fdff4bdba81e3ba9f533194eee1221838b9898f8
DonResnik <don.resnik@vistronix.com>
2016-03-24 21:08:05 -0400

    completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix - takes in a matrix, and sets up
+## a cache to store a value related to the matrix
+## that can be retrieved using internal getmatrix() method
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() 
+    setmatrix <- function(solve) m <<- solve
+    getmatrix <- function() m
+    list(set = set, get = get,
+         setmatrix = setmatrix,
+         getmatrix = getmatrix)
 }
 
+## cacheSolve - checks the cacheMatrix to see if the 
+## matrix already has a stored  result.  If it 
+## does it returns it from the cache and notifies the 
+## user that the result came from cached data.  If not, it runs
+## solve() on the matrix and stores the result in the cache.
 
-## Write a short comment describing this function
+## sample code to test the methods:
+## d <- matrix(c(10,0,9,6), nrow=2, ncol=2)
+## myMatrix <- makeCacheMatrix(d)
+## cacheSolve(myMatrix)
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getmatrix()
+        if(!is.null(m)) {
+          message("getting cached data")
+          return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setmatrix(m)
+        m
 }
